c271265166d660c6b52ba291eb0c090a7444a203
smsguy927 <smsguy927@gmail.com>
2021-05-17 12:46:32 -0500

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "John Doe";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}`
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`
